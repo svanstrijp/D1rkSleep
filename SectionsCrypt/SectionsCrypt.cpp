ff35798d5f16e54f4e644788bf960466b78c0778
Sander <17788939+svanstrijp@users.noreply.github.com>
2026-02-20 15:52:15 +0100

    fix

--- SectionsCrypt/SectionsCrypt.cpp
+++ SectionsCrypt/SectionsCrypt.cpp
@@ -1,188 +1,136 @@
 #define _CRT_RAND_S
 #include <Windows.h>
-#include <tlhelp32.h>
 
 #define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0)
-#define NtCurrentThread() (  ( HANDLE ) ( LONG_PTR ) -2 )
-#define NtCurrentProcess() ( ( HANDLE ) ( LONG_PTR ) -1 )
+#define NtCurrentThread() ((HANDLE)(LONG_PTR)-2)
+#define NtCurrentProcess() ((HANDLE)(LONG_PTR)-1)
 
 typedef struct {
-	DWORD	Length;
-	DWORD	MaximumLength;
-	PVOID	Buffer;
+    DWORD Length;
+    DWORD MaximumLength;
+    PVOID Buffer;
 } USTRING;
 
+VOID D1rkCrypt(DWORD SleepTime)
+{
+    DWORD OldProtect = 0;
 
-static VOID SuspendResumeOtherThreads(BOOL suspend) {
-    DWORD currentTid = GetCurrentThreadId();
-    DWORD pid = GetCurrentProcessId();
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (hSnapshot == INVALID_HANDLE_VALUE) return;
+    PVOID ImageBase = GetModuleHandle(NULL);
+    PIMAGE_DOS_HEADER pDOS = (PIMAGE_DOS_HEADER)ImageBase;
+    PIMAGE_NT_HEADERS pNT = (PIMAGE_NT_HEADERS)((DWORD64)ImageBase + pDOS->e_lfanew);
+    PIMAGE_SECTION_HEADER pSection = IMAGE_FIRST_SECTION(pNT);
 
-    THREADENTRY32 te32;
-    te32.dwSize = sizeof(THREADENTRY32);
-
-    if (Thread32First(hSnapshot, &te32)) {
-        do {
-            if (te32.th32OwnerProcessID == pid && te32.th32ThreadID != currentTid) {
-                HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, te32.th32ThreadID);
-                if (hThread) {
-                    if (suspend)
-                        SuspendThread(hThread);
-                    else
-                        ResumeThread(hThread);
-                    CloseHandle(hThread);
-                }
-            }
-        } while (Thread32Next(hSnapshot, &te32));
-    }
-    CloseHandle(hSnapshot);
-}
-
-
-VOID D1rkCrypt(DWORD SleepTime) {
-
-	DWORD   OldProtect = 0;
-
-	PVOID ImageBase = GetModuleHandle(NULL);
-	DWORD ImageSize = ((PIMAGE_NT_HEADERS)((DWORD64)ImageBase + ((PIMAGE_DOS_HEADER)ImageBase)->e_lfanew))->OptionalHeader.SizeOfImage;
-
-	IMAGE_DOS_HEADER* DOS_HEADER = (IMAGE_DOS_HEADER*)ImageBase;
-	IMAGE_NT_HEADERS* NT_HEADER = (IMAGE_NT_HEADERS*)((DWORD64)ImageBase + DOS_HEADER->e_lfanew);
-	IMAGE_SECTION_HEADER* SECTION_HEADER = IMAGE_FIRST_SECTION(NT_HEADER);
-
-	LPVOID txtSectionBase = (LPVOID)((DWORD64)ImageBase + (DWORD64)SECTION_HEADER->PointerToRawData);
-	DWORD txtSectionSize = SECTION_HEADER->SizeOfRawData;
-
-
-    LPVOID relocBase = NULL;
-    DWORD relocSize = 0;
-
-    for (int i = 0; i < NT_HEADER->FileHeader.NumberOfSections; i++) {
-        if (!strcmp(".reloc", (const char*)SECTION_HEADER->Name)) {
-            relocBase = (LPVOID)((DWORD64)ImageBase + (DWORD64)SECTION_HEADER->PointerToRawData);
-            relocSize = SECTION_HEADER->SizeOfRawData;
+    // === FIND .text SECTION ONLY (correct in-memory addressing) ===
+    LPVOID textBase = NULL;
+    DWORD  textSize = 0;
+    for (WORD i = 0; i < pNT->FileHeader.NumberOfSections; i++)
+    {
+        if (pSection->Characteristics & IMAGE_SCN_MEM_EXECUTE)
+        {
+            textBase = (LPVOID)((DWORD64)ImageBase + pSection->VirtualAddress);
+            textSize = pSection->Misc.VirtualSize;
+            break;
         }
-        SECTION_HEADER++;
+        pSection++;
+    }
+    if (textBase == NULL || textSize == 0) return;
+
+    CONTEXT CtxThread   = { 0 };
+    CONTEXT RopProtRW   = { 0 };
+    CONTEXT RopMemEnc   = { 0 };
+    CONTEXT RopDelay    = { 0 };
+    CONTEXT RopMemDec   = { 0 };
+    CONTEXT RopProtRX   = { 0 };
+    CONTEXT RopSetEvt   = { 0 };
+
+    HANDLE hTimerQueue = NULL;
+    HANDLE hNewTimer   = NULL;
+    HANDLE hEvent      = NULL;
+
+    CHAR KeyBuf[16];
+    unsigned int r = 0;
+    for (int i = 0; i < 16; i++) {
+        rand_s(&r);
+        KeyBuf[i] = (CHAR)r;
     }
 
-    DWORD CryptSize = ImageSize - (DWORD)((DWORD)txtSectionBase - (DWORD)ImageBase) 
-        - (ImageSize - ((DWORD)relocBase - (DWORD)ImageBase)) + relocSize;
-	
-	CONTEXT CtxThread = { 0 };
+    USTRING Key = { 0 };
+    USTRING Img = { 0 };
 
-	CONTEXT RopProtRW = { 0 };
-	CONTEXT RopMemEnc = { 0 };
-	CONTEXT RopDelay = { 0 };
-	CONTEXT RopMemDec = { 0 };
-	CONTEXT RopProtRX = { 0 };
-	CONTEXT RopSetEvt = { 0 };
+    PVOID NtContinue = GetProcAddress(GetModuleHandleA("ntdll"), "NtContinue");
+    PVOID SysFunc032 = GetProcAddress(LoadLibraryA("advapi32"), "SystemFunction032");
 
-	HANDLE  hTimerQueue = NULL;
-	HANDLE  hNewTimer = NULL;
-	HANDLE  hEvent = NULL;
+    Key.Buffer = KeyBuf;
+    Key.Length = Key.MaximumLength = 16;
 
+    Img.Buffer = textBase;
+    Img.Length = Img.MaximumLength = textSize;
 
-	CHAR KeyBuf[16];
-	unsigned int r = 0;
-	for (int i = 0; i < 16; i++) {
-		rand_s(&r);
-		KeyBuf[i] = (CHAR)r;
-
-	}
-
-	USTRING Key = { 0 };
-	USTRING Img = { 0 };
-
-	PVOID   NtContinue = NULL;
-	PVOID   SysFunc032 = NULL;
-
-	hEvent = CreateEventW(0, 0, 0, 0);
-
-	hTimerQueue = CreateTimerQueue();
-
-	NtContinue = GetProcAddress(GetModuleHandleA("Ntdll"), "NtContinue");
-	SysFunc032 = GetProcAddress(LoadLibraryA("Advapi32"), "SystemFunction032");
-
-	
-	Key.Buffer = KeyBuf;
-	Key.Length = Key.MaximumLength = 16;
-
-	Img.Buffer = txtSectionBase;
-	Img.Length = Img.MaximumLength = CryptSize;
+    hEvent      = CreateEventW(NULL, FALSE, FALSE, NULL);
+    hTimerQueue = CreateTimerQueue();
 
     if (CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)RtlCaptureContext, &CtxThread, 0, 0, WT_EXECUTEINTIMERTHREAD))
     {
-        WaitForSingleObject(hEvent, 0x32);
+        WaitForSingleObject(hEvent, 50);
 
         memcpy(&RopProtRW, &CtxThread, sizeof(CONTEXT));
         memcpy(&RopMemEnc, &CtxThread, sizeof(CONTEXT));
-        memcpy(&RopDelay, &CtxThread, sizeof(CONTEXT));
+        memcpy(&RopDelay,  &CtxThread, sizeof(CONTEXT));
         memcpy(&RopMemDec, &CtxThread, sizeof(CONTEXT));
         memcpy(&RopProtRX, &CtxThread, sizeof(CONTEXT));
         memcpy(&RopSetEvt, &CtxThread, sizeof(CONTEXT));
 
-        // VirtualProtect( ImageBase, ImageSize, PAGE_READWRITE, &OldProtect );
+        // 1. RW
         RopProtRW.Rsp -= 8;
         RopProtRW.Rip = (DWORD64)VirtualProtect;
-        RopProtRW.Rcx = (DWORD64)ImageBase;
-        RopProtRW.Rdx = ImageSize;
-        RopProtRW.R8 = PAGE_READWRITE;
-        RopProtRW.R9 = (DWORD64)&OldProtect;
+        RopProtRW.Rcx = (DWORD64)textBase;
+        RopProtRW.Rdx = textSize;
+        RopProtRW.R8  = PAGE_READWRITE;
+        RopProtRW.R9  = (DWORD64)&OldProtect;
 
-        // SystemFunction032( &Key, &Img );
+        // 2. Encrypt
         RopMemEnc.Rsp -= 8;
         RopMemEnc.Rip = (DWORD64)SysFunc032;
         RopMemEnc.Rcx = (DWORD64)&Img;
         RopMemEnc.Rdx = (DWORD64)&Key;
 
-        
-        // WaitForSingleObject( hTargetHdl, SleepTime );
+        // 3. Sleep (unhooked via process handle timeout)
         RopDelay.Rsp -= 8;
         RopDelay.Rip = (DWORD64)WaitForSingleObject;
         RopDelay.Rcx = (DWORD64)NtCurrentProcess();
         RopDelay.Rdx = SleepTime;
 
-        // SystemFunction032( &Key, &Img );
+        // 4. Decrypt
         RopMemDec.Rsp -= 8;
         RopMemDec.Rip = (DWORD64)SysFunc032;
         RopMemDec.Rcx = (DWORD64)&Img;
         RopMemDec.Rdx = (DWORD64)&Key;
 
-        
-        // VirtualProtect( ImageBase, ImageSize, PAGE_EXECUTE_READWRITE, &OldProtect );
+        // 5. RX
         RopProtRX.Rsp -= 8;
         RopProtRX.Rip = (DWORD64)VirtualProtect;
-        RopProtRX.Rcx = (DWORD64)ImageBase;
-        RopProtRX.Rdx = ImageSize;
-        RopProtRX.R8 = PAGE_EXECUTE_READWRITE;
-        RopProtRX.R9 = (DWORD64)&OldProtect;
+        RopProtRX.Rcx = (DWORD64)textBase;
+        RopProtRX.Rdx = textSize;
+        RopProtRX.R8  = PAGE_EXECUTE_READ;
+        RopProtRX.R9  = (DWORD64)&OldProtect;
 
-
+        // 6. Signal done
         RopSetEvt.Rsp -= 8;
         RopSetEvt.Rip = (DWORD64)SetEvent;
         RopSetEvt.Rcx = (DWORD64)hEvent;
 
-        // Suspend all other threads before encrypting executable sections.
-        // This prevents CLR threads (GC, finalizer, JIT, threadpool) from
-        // executing code in encrypted memory regions during the sleep window.
-        SuspendResumeOtherThreads(TRUE);
-
         CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopProtRW, 100, 0, WT_EXECUTEINTIMERTHREAD);
         CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopMemEnc, 200, 0, WT_EXECUTEINTIMERTHREAD);
-        CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopDelay, 300, 0, WT_EXECUTEINTIMERTHREAD);
+        CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopDelay,  300, 0, WT_EXECUTEINTIMERTHREAD);
         CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopMemDec, 400, 0, WT_EXECUTEINTIMERTHREAD);
         CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopProtRX, 500, 0, WT_EXECUTEINTIMERTHREAD);
         CreateTimerQueueTimer(&hNewTimer, hTimerQueue, (WAITORTIMERCALLBACK)NtContinue, &RopSetEvt, 600, 0, WT_EXECUTEINTIMERTHREAD);
 
         WaitForSingleObject(hEvent, INFINITE);
-
-        // Resume all other threads now that sections are decrypted and RX again
-        SuspendResumeOtherThreads(FALSE);
     }
-    DeleteTimerQueue(hTimerQueue);
-}
 
+    if (hTimerQueue) DeleteTimerQueueEx(hTimerQueue, NULL);
+}
 
 extern "C" __declspec(dllexport) VOID WINAPI D1rkSleep(DWORD dwMilliseconds)
 {
@@ -196,8 +144,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hModule);
-        break;
-    case DLL_PROCESS_DETACH:
         break;
     }
     return TRUE;
